6dea9ded6c4f1f26651dd816c389c09cad7b45be
sai-karthik-9 <139197776+sai-karthik-9@users.noreply.github.com>
2024-05-23 19:22:32 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,39 @@
-import { Component } from "react";
+import React, { useState, useMemo } from "react";
 import FoodBox from "./components/FoodBox/FoodBox";
 import FoodData from "./components/FoodData";
 import Search from "./components/Search/Search";
 import "./App.css";
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  const [search, setSearch] = useState("");
+  const [count, setCount] = useState(0);
 
-    this.state = {
-      search: "",
-      count: 0,
-    };
-  }
-
-  handleSearch = (query) => {
-    this.setState({ search: query });
+  const handleSearch = (query) => {
+    setSearch(query);
   };
 
-  handleCount = (value) => {
-    this.setState({ count: value });
+  const handleCount = (value) => {
+    setCount(value);
   };
 
-  render() {
-    return (
-      <div className="app">
-        <Search handleSearch={this.handleSearch} />
+  const filteredFoodData = useMemo(() => {
+    if (!search) return FoodData;
 
-        <div className="items">
-          {FoodData.filter((el) => {
-            if (this.state.search == "") return el;
-            else if (
-              el.name.toLowerCase().includes(this.state.search.toLowerCase())
-            ) {
-              return el;
-            }
-          }).map((food) => (
-            <FoodBox data={food} key={food.id} />
-          ))}
-        </div>
-      </div>
+    return FoodData.filter((food) =>
+      food.name.toLowerCase().includes(search.toLowerCase())
     );
-  }
-}
+  }, [search]);
+
+  return (
+    <div className="app">
+      <Search handleSearch={handleSearch} />
+      <div className="items">
+        {filteredFoodData.map((food) => (
+          <FoodBox data={food} key={food.id} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
